9b7fb3f020c5b2b859bd726fe08f6dd594a15068
ajwad-abrar <ajwadabrar@iut-dhaka.edu>
2023-03-19 05:16:00 +0600

    Aded Form for register and login

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Sociopedia
+          Bondhutto
         </Typography>
       </Box>
 
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media for Sociopaths!
+          Welcome to Bondhutto, the Social Media for meeting people!
         </Typography>
         <Form />
       </Box>
